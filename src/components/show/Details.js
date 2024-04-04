e27df42baad6d92d893f3f1c7a94c4ea757636b3
AdityaPare <adityapare2001@gmail.com>
2024-04-04 16:52:44 +0530

    styed show page

---
+++ src/components/show/Details.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { DetailsWrapper } from './Details.style';
+
+const Details = ({ status, premiered, network }) => {
+  return (
+    <DetailsWrapper>
+      <p>
+        Status: <span>{status}</span>
+      </p>
+      <p>
+        Premiered {premiered} {network ? `on ${network.name}` : null}
+      </p>
+    </DetailsWrapper>
+  );
+};
+
+export default Details;
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,12 @@
+/* eslint-disable no-undef */
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
+import ShowMainData from '../components/show/ShowMainData';
+import Details from '../components/show/Details';
+import Season from '../components/show/Seasons';
+import Cast from '../components/show/Cast';
+import { InfoBlock, ShowPageWrapper } from './Show.styled';
 
 const reducer = (prevState, action) => {
   switch (action.type) {
@@ -58,7 +64,35 @@ function Show() {
     return <div>Error occured:{error}</div>;
   }
 
-  return <div>This is show page</div>;
+  return (
+    <ShowPageWrapper>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+
+      <InfoBlock>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </InfoBlock>
+
+      <InfoBlock>
+        <h2>Season</h2>
+        <Season seasons={show._embedded.seasons} />
+      </InfoBlock>
+      <InfoBlock>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </InfoBlock>
+    </ShowPageWrapper>
+  );
 }
 
 export default Show;
