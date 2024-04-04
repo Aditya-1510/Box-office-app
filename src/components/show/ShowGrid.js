5bafb4837f585a35343b8461e702d308099e00f2
AdityaPare <adityapare2001@gmail.com>
2024-04-04 18:15:16 +0530

    usign cusrom hooks for starred page

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import ShowCard from './ShowCard';
-import IMAGE_NOT_FOUND from '../../images/not-found.png';
+
 import { FlexGrid } from '../styled';
-function ShowGrid({ data }) {
+
+import IMAGE_NOT_FOUND from '../../images/not-found.png';
+import { useShow } from '../../misc/custome-hooks';
+
+const ShowGrid = ({ data }) => {
+  const [starredShows, dispatchStarred] = useShow();
+
   return (
     <FlexGrid>
-      {data.map(({ show }) => (
-        <ShowCard
-          key={show.id}
-          id={show.id}
-          name={show.name}
-          image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
-          summary={show.summary}
-        />
-      ))}
+      {data.map(({ show }) => {
+        const isStarred = starredShows.includes(show.id);
+
+        const onstarredClick = () => {
+          if (isStarred) {
+            dispatchStarred({ type: 'REMOVE', showId: show.id });
+          } else {
+            dispatchStarred({ type: 'ADD', showId: show.id });
+          }
+        };
+
+        return (
+          <ShowCard
+            key={show.id}
+            id={show.id}
+            name={show.name}
+            image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
+            summary={show.summary}
+            onstarredClick={onstarredClick}
+            isStarred={isStarred}
+          />
+        );
+      })}
     </FlexGrid>
   );
-}
+};
 
 export default ShowGrid;
